2a1f2331af145b4baae7b5dfddad099435d2a663
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-07-06 14:45:12 +0100

    added process for mongodb

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ReactFetch');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
